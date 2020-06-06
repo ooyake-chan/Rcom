dbc970895b9e0eca9b29a90c8a75d122fde413f5
ooyake_chan <tendouzi1105@gmail.com>
2020-06-06 20:05:09 +0900

    パンくずリストを修正途中

--- single.php
+++ single.php
@@ -1,4 +1,47 @@
 <?php get_header(); ?> 
+<main>
+    <div class="header">
+    <div class="wrapper">
+      <div class="bread">
+        <ol itemscope itemtype="https://schema.org/BreadcrumbList">
+          <!-- 1つめ -->
+          <li itemprop="itemListElement" itemscope
+              itemtype="https://schema.org/ListItem">
+            <a itemprop="item" href="<?php echo home_url(); ?>">
+                <span itemprop="name">ホーム</span>
+            </a>
+            <meta itemprop="position" content="1" />
+          </li>
+        
+          <!-- 2つめ -->
+          <li itemprop="itemListElement" itemscope
+              itemtype="https://schema.org/ListItem">
+            <a itemprop="item" href="<?php echo home_url('/'); ?>2020">
+                <span itemprop="name">お知らせ</span>
+            </a>
+            <meta itemprop="position" content="2" />
+          </li>
+          <!-- 3つめ -->
+          <li itemprop="itemListElement" itemscope
+                itemtype="https://schema.org/ListItem">
+              <a itemprop="item" href="#">
+                  <span itemprop="name">2020年</span>
+              </a>
+              <meta itemprop="position" content="2" />
+            </li>
+        </ol>
+      </div>
+      <div class="clear"></div><!-- パンくずリスト -->
+      <h1><?php the_title(); ?></h1>
+<!-- カテゴリの説明文 -->
+<?php if (is_category() && //カテゴリページの時
+          !is_paged() &&   //カテゴリページのトップの時
+          category_description()) : //カテゴリの説明文が空でない時 ?>
+<div class="category-description"><?php echo category_description(); ?></div>
+<?php endif; ?>
+    </div>
+    </div><!-- header -->
+
     <div class="conteiner wrapper contents">
     <div class="contents-menu">
         <ul class="data-menu">
