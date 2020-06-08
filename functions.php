dcb5ee4bdfebf71fd700cd147254739a5aa43273
ooyake_chan <tendouzi1105@gmail.com>
2020-06-08 17:30:53 +0900

    セキュリティ対策（情報を隠す）

--- functions.php
+++ functions.php
@@ -10,4 +10,16 @@
  
 add_filter( 'wp_get_attachment_url', 'delete_domain_from_attachment_url' );
 
+
+remove_action('wp_head', 'wp_generator');// WordPressのバージョン
+remove_action('wp_head', 'wp_shortlink_wp_head');// 短縮URLのlink
+remove_action('wp_head', 'wlwmanifest_link');// ブログエディターのマニフェストファイル
+remove_action('wp_head', 'rsd_link');// 外部から編集するためのAPI
+remove_action('wp_head', 'feed_links_extra', 3);// フィードへのリンク
+remove_action('wp_head', 'print_emoji_detection_script', 7);// 絵文字に関するJavaScript
+remove_action('wp_head', 'rel_canonical');// カノニカル
+remove_action('wp_print_styles', 'print_emoji_styles');// 絵文字に関するCSS
+remove_action('admin_print_scripts', 'print_emoji_detection_script');// 絵文字に関するJavaScript
+remove_action('admin_print_styles', 'print_emoji_styles');// 絵文字に関するCSS
+
 ?>
